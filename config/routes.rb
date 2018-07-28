8e2568aeec5f6dfc5a9b5e7f19bbc00df92eba5f
Hugo Collado <hugo.collado91@gmail.com>
2018-07-28 09:51:20 +0800

    scalfold improvement with new migration to add chef and new method to show top restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      get 'top', to: "restaurants#top"
+    end
+    member do
+      get 'chef', to: "restaurants#chef"
+    end
+  end
+  root to: 'restaurants#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    description:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    stars:        5
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    stars:        4
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
